a1e548d81c5784bb3f2d86ab8753180192b590f9
hyunbin1 <kimgusqls1@gmail.com>
2021-04-01 12:44:38 +0900

    add/globalStyles.js / errer/font not accepted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
     <Router>
+      <GlobalStyle />
       {/* bavbar.js 가져오기 - 파일명과 상관없이 <Navbar  /> 써주기 + import Navbar*/}
       <Navbar />
     </Router>
